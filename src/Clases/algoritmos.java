ff35f3028f1e4e197a34eab2b5e6995f51d6e1fb
horactis <horactis5@gmail.com>
2025-05-09 11:12:05 +0200

    actualizado

--- src/Clases/algoritmos.java
+++ src/Clases/algoritmos.java
@@ -1,5 +1,10 @@
 package Clases;
 
 public abstract class algoritmos {
-
+	public int fibonacci (int numero) {
+		if (numero < 0) {
+			throw new IllegalArgumentException("el numero debe de ser positivo");
+		}
+		return ;
+	}
 }
